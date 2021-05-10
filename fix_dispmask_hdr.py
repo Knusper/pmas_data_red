8fd55453c11ab15ce3b196a434e7ee96e05bd16e
Edmund Christian Herenz <knusper2000@googlemail.com>
2021-05-10 17:00:32 -0400

    fix_dismpa_hdr.py - fixes problems with ldmask.sh
    p3d version 2.6.X until version ??? has a problem
    with reading input dispersion masks properly (which we may use in ldmask.sh).
    fix_dispmap_hdr.py fixes this

---
+++ fix_dispmask_hdr.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+#  p3d version 2.6.X until version ??? has a problem
+#  with reading input dispersion masks properly (which we may use in ldmask.sh).
+#  The following python lines fix this.
+
+import sys
+from astropy.io import fits
+
+filename = sys.argv[1]
+
+hdu = fits.open(filename)
+
+params_header = hdu['PARAMS'].header
+
+for key in params_header.keys():
+    if key.startswith('ARCL') and not key.startswith('ARCLN'):
+        print(key,params_header[key])
+        num = int(key[-4:])
+        num_str = 'ARCLN'+str(num).zfill(3)
+        params_header[num_str] = params_header[key]
+
+hdu.writeto(filename[:-4]+'hdrfix.fits')
